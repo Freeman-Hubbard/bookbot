0ed5c2ecc5112cc43091535c7cd092a353b29059
Freeman-Hubbard <freemanh.dev@protonmail.com>
2023-09-09 18:56:54 -0400

    wrote functions to generate word count, alphabet count, and a report based on the returns of those functions

---
+++ main.py
@@ -0,0 +1,36 @@
+with open('books/frankenstein.txt') as f:
+    book = f.read()
+
+def count_words(string):
+    return len(string.split())
+
+def count_letters(string):
+    letter_count = {}
+    words = string.lower().split()
+    for word in words:
+        for letter in word:
+            if letter.isalpha():
+                if letter not in letter_count:
+                    letter_count[letter] = 1
+                else:
+                    letter_count[letter] += 1
+    return letter_count
+
+# letter to list -> sort letters -> back to dict/object
+letter_list = list(count_letters(book).items())
+letter_list.sort(key=lambda x:x[0])
+letter_dict = dict(letter_list)
+
+# for letter in letter_dict:
+#     print(letter)
+# letterList = {k : v for k, v in sorted(letter_count.items())}
+
+def report(string):
+    print(f"--- Begin report of book ---")
+    print(f"{count_words(string)} words found in the document")
+
+    for letter in letter_dict:
+        print(f"The '{letter}' character was found {letter_dict[letter]} times")
+    print("--- End report ---")
+
+report(book)
